cbbc253bb9de4efe4073bafdf4c6ddcf68898492
a135151 <135151@stud.hvl.no>
2025-02-21 15:24:42 +0100

    gjort om på Innsetting

--- DAT102Oblig2/src/sortering/Innsetting.java
+++ DAT102Oblig2/src/sortering/Innsetting.java
@@ -4,61 +4,76 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class Innsetting {
-    
-    public static void innsettingSortmod(int[] a) {
-        int n = a.length;
-        
-        int minIndex = 0;
-        for (int i = 1; i < n; i++) {
-            if (a[i] < a[minIndex]) {
-                minIndex = i;
-            }
-        }
-        byttElement(a, 0, minIndex);
-        
-        for (int i = 2; i < n - 1; i += 2) {
-            settinnToElement(a, i, i + 1);
-        }
-        
-        if (n % 2 == 0) {
-            settinnElement(a, n - 1);
-        }
-    }
-    
-    private static void settinnToElement(int[] a, int i, int j) {
-        settinnElement(a, i);
-        settinnElement(a, j);
-    }
-    
-    private static void settinnElement(int[] a, int i) {
-        int temp = a[i];
-        int j = i - 1;
-        while (a[j] > temp) {
-            a[j + 1] = a[j];
-            j--;
-        }
-        a[j + 1] = temp;
-    }
-    
-    public static void byttElement(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-    
+
+	public static <T extends Comparable<? super T>> void innsettingSortmod(T[] a) {
+
+			int n = a.length;
+
+		    int minIndex = 0;
+		    for (int i = 1; i < n; i++) {
+		        if (a[i].compareTo(a[minIndex]) < 0) {
+		            minIndex = i;
+		        }
+		    }
+		    bytt(a, minIndex, 0);
+
+		    for (int i = 1; i < n; i += 2) {
+		        if (i + 1 < n) {
+		            T minste = a[i];
+		            T storste = a[i + 1];
+
+		            if (minste.compareTo(storste) > 0) {
+		                T temp = minste;
+		                minste = storste;
+		                storste = temp;
+		            }
+
+		            int j = i - 1;
+		            
+		            while (a[j].compareTo(storste) > 0) {
+		                a[j + 2] = a[j];
+		                j--;
+		            }
+		            a[j + 2] = storste;
+
+		            j = i - 1;
+		            
+		            while (a[j].compareTo(minste) > 0) {
+		                a[j + 1] = a[j];
+		                j--;
+		            }
+		            a[j + 1] = minste;
+		        } else {
+		            int j = i - 1;
+		            T sisteElement = a[i];
+		            while (a[j].compareTo(sisteElement) > 0) {
+		                a[j + 1] = a[j];
+		                j--;
+		            }
+		            a[j + 1] = sisteElement;
+		        }
+		    }
+		}
+
+		private static <T> void bytt(T[] array, int i, int j) {
+		    T temp = array[i];
+		    array[i] = array[j];
+		    array[j] = temp;
+		}
+
     public static void main(String[] args) {
         Random tilfeldig = new Random();
-        int n = 50000;
-        int antall = 10;
-        
-        int[][] a = new int[antall][n];
-        
+        int n = 100000;
+        int antall = 5;
+
+        Integer[][] a = new Integer[antall][n];
+
         for (int i = 0; i < antall; i++) {
             for (int j = 0; j < n; j++) {
                 a[i][j] = tilfeldig.nextInt();
             }
         }
-        
+
         long totalTid = 0;
         for (int i = 0; i < antall; i++) {
             long start = System.currentTimeMillis();
@@ -66,8 +81,9 @@ public class Innsetting {
             long slutt = System.currentTimeMillis();
             totalTid += (slutt - start);
         }
-        
+
         System.out.println("Total tid: " + totalTid + " ms");
         System.out.println("Gjennomsnittlig tid: " + (totalTid / antall) + " ms");
+
     }
 }
--- DAT102Oblig2/src/sortering/InnsettingUmodifisert.java
+++ DAT102Oblig2/src/sortering/InnsettingUmodifisert.java
@@ -4,34 +4,35 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class InnsettingUmodifisert {
-    
-    public static void innsettingSort(int[] a) {
-        int n = a.length;
-        
-        for (int i = 1; i < n; i++) {
-            int temp = a[i];
-            int j = i - 1;
-            while (j >= 0 && a[j] > temp) {
-                a[j + 1] = a[j];
-                j--;
-            }
-            a[j + 1] = temp;
-        }
-    }
-    
+
+	public static <T extends Comparable<? super T>> void innsettingSort(T[] a) {
+	    int n = a.length;
+
+	    for (int i = 1; i < n; i++) {
+	        T temp = a[i];
+	        int j = i - 1;
+
+	        while (j >= 0 && a[j].compareTo(temp) > 0) {
+	            a[j + 1] = a[j];
+	            j--;
+	        }
+	        a[j + 1] = temp;
+	    }
+	}
+
     public static void main(String[] args) {
         Random tilfeldig = new Random();
-        int n = 50000;
-        int antall = 10;
-        
-        int[][] a = new int[antall][n];
-        
+        int n = 100000;
+        int antall = 5;
+
+        Integer[][] a = new Integer[antall][n];
+
         for (int i = 0; i < antall; i++) {
             for (int j = 0; j < n; j++) {
                 a[i][j] = tilfeldig.nextInt();
             }
         }
-        
+
         long totalTid = 0;
         for (int i = 0; i < antall; i++) {
             long start = System.currentTimeMillis();
@@ -39,7 +40,7 @@ public class InnsettingUmodifisert {
             long slutt = System.currentTimeMillis();
             totalTid += (slutt - start);
         }
-        
+
         System.out.println("Total tid: " + totalTid + " ms");
         System.out.println("Gjennomsnittlig tid: " + (totalTid / antall) + " ms");
     }
